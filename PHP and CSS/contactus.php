59e3842fa439b223b2828bf4405630ce0ebc7477
Soham Salkar <63644015+sohamsalkar@users.noreply.github.com>
2021-05-24 22:43:07 +0530

    Update contactus.php

--- PHP and CSS/contactus.php
+++ PHP and CSS/contactus.php
@@ -92,11 +92,11 @@ $id=$_SESSION["username"];
                     <h2><i class="fa fa-handshake-o" aria-hidden="true"></i>  Contact Info</h2>
                     <div class="container">
                     <ul>
-                        <li><h3><i class="fa fa-map-marker" aria-hidden="true"></i>  Airoli,Navi Mumbai-708.</h3>
+                        <li><h3><i class="fa fa-map-marker" aria-hidden="true"></i> Mumbai-708.</h3>
                         </li>
-                        <li><h3><i class="fa fa-envelope-o" aria-hidden="true"></i>  ritiesh3542@gmail.com
+                        <li><h3><i class="fa fa-envelope-o" aria-hidden="true"></i>  salkarsoham2@gmail.com
                         </li>
-                        <li><h3><i class="fa fa-phone" aria-hidden="true"></i>   23657577
+                        <li><h3><i class="fa fa-phone" aria-hidden="true"></i> 1234-123-123
                         </li>
                     </ul>
                 </div>
